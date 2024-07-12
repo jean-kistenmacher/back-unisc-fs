46e764121f3871926ae22d2452f5825c0d7276e6
Jean Kistenmacher <jean.kistenmacher@b2wdigital.com>
2024-07-12 15:32:58 -0300

    feat: gerar pdf's

--- src/main/java/unisc/fs/controller/PedidoController.java
+++ src/main/java/unisc/fs/controller/PedidoController.java
@@ -4,16 +4,22 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
 import unisc.fs.dto.Sessao;
 import unisc.fs.entity.PedidoEntity;
 import unisc.fs.service.PedidoService;
-
+import java.io.IOException;
 import java.util.UUID;
+
+
+
 
 @Path("/pedidos")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class PedidoController {
+
+    private static final Logger LOGGER = Logger.getLogger(PedidoController.class);
 
     private final PedidoService pedidoService;
 
@@ -66,4 +72,23 @@ public class PedidoController {
         return Response.ok(pedidoService.findPedidoBySessao(sessao)).build();
     }
 
+    @GET
+    @Path("/comprovante/{id}")
+    @Produces("application/pdf")
+    public Response gerarComprovantePDF(@PathParam("id") UUID pedidoId) throws IOException {
+        byte[] pdfBytes = pedidoService.gerarComprovantePDF(pedidoId);
+        return Response.ok(pdfBytes)
+                .header("Content-Disposition", "attachment; filename=boleto.pdf")
+                .build();
+    }
+
+    @GET
+    @Path("/boleto/{id}")
+    @Produces("application/pdf")
+    public Response gerarBoletoPDF(@PathParam("id") UUID pedidoId) throws IOException {
+        byte[] pdfBytes = pedidoService.gerarBoletoPDF(pedidoId);
+        return Response.ok(pdfBytes)
+                .header("Content-Disposition", "attachment; filename=boleto.pdf")
+                .build();
+    }
 }
--- src/main/java/unisc/fs/service/PedidoService.java
+++ src/main/java/unisc/fs/service/PedidoService.java
@@ -1,19 +1,34 @@
 package unisc.fs.service;
 
+import com.itextpdf.kernel.colors.DeviceGray;
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.properties.TextAlignment;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.persistence.EntityManager;
+
 import unisc.fs.dto.Sessao;
 import unisc.fs.entity.PedidoEntity;
 import unisc.fs.exception.PedidoNotFoundException;
 import unisc.fs.repository.PedidoRepository;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+
+import org.jboss.logging.Logger;
 
 @ApplicationScoped
 public class PedidoService {
 
     private final PedidoRepository pedidoRepository;
+
+    private static final Logger LOGGER = Logger.getLogger(PedidoService.class);
 
     public PedidoService(PedidoRepository pedidoRepository) {
         this.pedidoRepository = pedidoRepository;
@@ -51,5 +66,92 @@ public class PedidoService {
 
     public List<PedidoEntity> findPedidoBySessao(Sessao sessao){
         return pedidoRepository.find("filme = ?1 and sessao = ?2 and date = ?3",sessao.getFilme(), sessao.getSessao(), sessao.getDate()).list();
+    }
+
+    public byte[] gerarComprovantePDF(UUID pedidoId) throws IOException {
+        var pedido  = findById(pedidoId);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PdfWriter writer = new PdfWriter(outputStream);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        document.add(new Paragraph("Comprovante de Compra")
+                .setFontColor(new DeviceRgb(0, 0, 255))
+                .setBold()
+                .setFontSize(20)
+                .setTextAlignment(TextAlignment.CENTER)
+                .setMarginBottom(20));
+
+        document.add(new Paragraph("Email do Cliente: " + pedido.getUsuario())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Filme: " + pedido.getFilme())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Sessão: " + pedido.getSessao())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Poltrona: " + pedido.getPoltrona())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Data: " + pedido.getDate())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+
+
+        document.close();
+
+        return outputStream.toByteArray();
+    }
+
+    public byte[] gerarBoletoPDF(UUID pedidoId) throws IOException {
+        var pedido  = findById(pedidoId);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PdfWriter writer = new PdfWriter(outputStream);
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+
+        document.add(new Paragraph("BOLETO")
+                .setFontColor(new DeviceRgb(0, 0, 255))
+                .setBold()
+                .setFontSize(20)
+                .setTextAlignment(TextAlignment.CENTER)
+                .setMarginBottom(20));
+
+        document.add(new Paragraph("Email do Cliente: " + pedido.getUsuario())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Filme: " + pedido.getFilme())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Sessão: " + pedido.getSessao())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.add(new Paragraph("Valor: R$" + pedido.getPreco())
+                .setFont(PdfFontFactory.createFont())
+                .setFontSize(12)
+                .setMarginBottom(5));
+
+        document.close();
+
+        return outputStream.toByteArray();
     }
 }
